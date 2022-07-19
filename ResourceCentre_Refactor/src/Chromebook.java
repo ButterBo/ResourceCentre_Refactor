7aae2c752abcbcfbdef5de9808c01dc4739e577c
21018812 <21018812@myrp.edu.sg>
2022-07-19 14:31:10 +0800

    b

--- ResourceCentre_Refactor/src/Chromebook.java
+++ ResourceCentre_Refactor/src/Chromebook.java
@@ -12,13 +12,9 @@ public class Chromebook extends Item{
 	}
 	
 	public String toString(){
-
-
 		// Write your codes here
-
 		String output = super.toString();
 		output = String.format("%-63s %-20d", output,os);
-
 		return output;
 	}
 }
