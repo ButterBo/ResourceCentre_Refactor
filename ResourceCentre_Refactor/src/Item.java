aa7de0ae7241f7c46ced482731c9d32e174ebff6
21010860 <21010860@myrp.edu.sg>
2022-07-19 14:16:49 +0800

    commit

--- ResourceCentre_Refactor/src/Item.java
+++ ResourceCentre_Refactor/src/Item.java
@@ -1,4 +1,3 @@
-
 public class Item {
 	private String assetTag;
 	private String description;
@@ -12,10 +11,23 @@ public class Item {
 		this.isAvailable = true;
 	}
 	public String toString() {
-		
 		// Write your codes here
-		return null;
+		String itemInfo = String.format("%-10s %-30s %-10s %-10s", assetTag, description, showAvailability(isAvailable), dueDate);
+		return itemInfo;
 	}
+	
+	public String showAvailability(boolean isAvailable) {
+		String avail;
+		
+		if (isAvailable == true) {
+			avail = "Yes";
+			
+		} else {
+			avail = "No";
+		}
+		return avail;
+	}
+
 	public String getAssetTag() {
 		return assetTag;
 	}
@@ -40,4 +52,3 @@ public class Item {
 		this.isAvailable = isAvailable;
 	}
 }
-
